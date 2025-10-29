559dab7df4cb9bf3a424ae0226fd30b1da279302
Mark Scherer <dereuromark@users.noreply.github.com>
2025-10-29 02:40:55 +0100

    Merge pull request #6 from dereuromark/coverage
    Add coverage.

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 DRIVER=$1;
+shift;
 
 echo "Starting PHPUNIT tests"
 export DB_DRIVER=$DRIVER
 
-vendor/bin/phpunit
+if [[ "$@" == *"--with-coverage"* ]]; then
+    vendor/bin/phpunit --coverage-clover=coverage.xml
+else
+    vendor/bin/phpunit
+fi
